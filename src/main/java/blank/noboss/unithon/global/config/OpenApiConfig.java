648e4da5da39748a49621ecebcd4ba907524e250
kmg22 <ssukmg22@gmail.com>
2026-08-25 22:02:54 +0900

    fix: Swagger mixed content 오류 해결

---
+++ src/main/java/blank/noboss/unithon/global/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package blank.noboss.unithon.global.config;
+
+import java.util.List;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .servers(List.of(new Server().url("/")));
+    }
+}
