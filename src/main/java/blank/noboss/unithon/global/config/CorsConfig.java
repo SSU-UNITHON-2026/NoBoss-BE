c5eb151b535b3d7a0ff517d362e652797fae5db5
kmg22 <ssukmg22@gmail.com>
2026-08-25 20:45:45 +0900

    fix: align cors property key

--- src/main/java/blank/noboss/unithon/global/config/CorsConfig.java
+++ src/main/java/blank/noboss/unithon/global/config/CorsConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Value("#{'${spring.cors.allowed-origins}'.split(',')}")
+    @Value("#{'${cors.allowed-origins}'.split(',')}")
     private List<String> allowedOrigins;
 
     @Override
