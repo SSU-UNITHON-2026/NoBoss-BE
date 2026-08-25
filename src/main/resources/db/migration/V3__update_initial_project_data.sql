62ea99750da67c55821c5d9519d80f09317bc09e
kmg22 <ssukmg22@gmail.com>
2026-08-26 00:00:50 +0900

    feat: 프로젝트 초기 목업 데이터 변경

---
+++ src/main/resources/db/migration/V3__update_initial_project_data.sql
@@ -0,0 +1,7 @@
+UPDATE projects
+SET team_name = 'B_LANK',
+    subject_name = '서비스디자인 캡스톤',
+    project_topic = '캠퍼스 중고거래 앱 UX 개선',
+    deadline = DATE '2026-12-11',
+    description = '교내 중고거래 과정의 불편함을 개선하는 UX 프로젝트'
+WHERE id = 1;
